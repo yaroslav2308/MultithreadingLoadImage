69676422a04169e9a29568efeb85c8cdee0284d0
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-08-03 20:46:00 +0300

    Create all components

--- MultithreadingLoadImage/LoadImageViewController.swift
+++ MultithreadingLoadImage/LoadImageViewController.swift
@@ -11,8 +11,32 @@ class LoadImageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .lightGray
     }
+    
+    let randomImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    let randomButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("RANDOM", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        return button
+    }()
+    
+    let slider: UISlider = {
+        let slider = UISlider()
+        slider.value = 0
+        
+        return slider
+    }()
+    
+//    private func setupLayout() {
+//        
+//    }
 
 
 }
